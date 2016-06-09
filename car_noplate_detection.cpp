93f0f2f1e7a6d763f95ae7c4df8034209d47c714
rohit <rohit.jagtap@gslab.com>
2016-06-09 19:57:29 +0530

    ROOM-2 : No_plate detected in video.

--- car_noplate_detection.cpp
+++ car_noplate_detection.cpp
@@ -32,11 +32,11 @@ int main(int argc, char* argv[]){
 	}
 
 
-//	VideoCapture capture(argv[1]);
-//	if (!capture.isOpened()) {
-//		cout<<"Error while opening camera"<<endl;
-//		return 1;
-//	}
+	VideoCapture capture(argv[1]);
+	if (!capture.isOpened()) {
+		cout<<"Error while opening camera"<<endl;
+		return 1;
+	}
 	//	capture.set(CV_CAP_PROP_BUFFERSIZE, 500);
 	Mat src_img, gray_img, resize_src_img;
 
@@ -45,18 +45,19 @@ int main(int argc, char* argv[]){
 	vector<Rect> cars;
 	bool count = true;
 	Rect crop;	//To create resize the grabbed faces and store the result  in this variable.
-//	int faceCount = 0;
-//	char imageToSave[50];	
-//	string folderName = "Faces";
-//	string folderRemoveCommand = "rm -rf" + folderName;
-//	system(folderRemoveCommand.c_str());	//Remove the folder if already present
-//	string folderCreateCommand = "mkdir " + folderName;
-//	system(folderCreateCommand.c_str());	//Create the folder if not present.
+	int CarCount = 0;
+	char imageToSave[50];	
+	string folderName = "Cars_Plates";
+	string folderRemoveCommand = "rm -rf" + folderName;
+	system(folderRemoveCommand.c_str());	//Remove the folder if already present
+	string folderCreateCommand = "mkdir " + folderName;
+	system(folderCreateCommand.c_str());	//Create the folder if not present.
 
 
-//	while(capture.read(src_img)){
-src_img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-{
+	while(capture.isOpened()){
+capture.read(src_img);
+//src_img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+//{
 		//for(;;){
 		//              capture.retrieve(src_img);
 		//                capture>>src_img;
@@ -77,21 +78,54 @@ src_img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 		//clock_t begin = clock();
 cout<<"width "<< src_img.size().width << endl;
 cout<<"height "<< src_img.size().height << endl;
-		if(src_img.size().width/2 > 600 && src_img.size().height/2 > 350)
-		{
-		resize(src_img, resize_src_img, Size(src_img.size().width/2, src_img.size().height/2) );
-		}
-		else
-		{
-		resize(src_img, resize_src_img, Size(src_img.size().width/1, src_img.size().height/1) );
-		}
+//		if(src_img.size().width/2 > 600 && src_img.size().height/2 > 350)
+//		{
+//		resize(src_img, resize_src_img, Size(src_img.size().width/1, src_img.size().height/1) );
+//		}
+//		else
+//		{
+//		resize(src_img, resize_src_img, Size(src_img.size().width/1, src_img.size().height/1) );
+//		}
 
-		cvtColor(resize_src_img,gray_img,CV_BGR2GRAY);
+		cvtColor(src_img,gray_img,CV_BGR2GRAY);
 		equalizeHist(gray_img,gray_img);
 		//              face_cascade.detectMultiScale(gray_img, faces, 1.1, 2, CV_HAAR_SCALE_IMAGE | CV_HAAR_DO_CANNY_PRUNING, cvSize(0,0), cvSize(500,500));
-		car_cascade.detectMultiScale(gray_img, cars, 1.11, 4, 0 | CASCADE_SCALE_IMAGE, Size(0,0));
+		car_cascade.detectMultiScale(gray_img, cars, 1.11, 2, 0 | CASCADE_SCALE_IMAGE, Size(0,0));
 
 		for(int i = 0; i<cars.size(); i++){
+
+
+
+
+//                if((cars[i].width + 95) <= src_img.size().width)
+//                        cars[i].width +=95;
+//                else if((cars[i].width + 85) <= src_img.size().width)
+//                        cars[i].width +=85;
+//                else if((cars[i].width + 75) <= src_img.size().width)
+//                        cars[i].width +=75;
+		if((cars[i].width + 65) <= src_img.size().width- 20)
+                	cars[i].width +=65;
+		else if((cars[i].width + 55) <= src_img.size().width-20)
+			cars[i].width +=55;
+                else if((cars[i].width + 45) <= src_img.size().width-20)
+                        cars[i].width +=45;
+                else if((cars[i].width + 35) <= src_img.size().width-20)
+                        cars[i].width +=35;
+                else if((cars[i].width + 25) <= src_img.size().width-20)
+                        cars[i].width +=25;
+                else if((cars[i].width + 15) <= src_img.size().width -20)
+                        cars[i].width +=15;
+
+
+		if((cars[i].height + 20) <= src_img.size().height)
+			cars[i].height +=20;
+                else if((cars[i].height + 15) <= src_img.size().height)
+                        cars[i].height +=15;
+                else if((cars[i].height + 10) <= src_img.size().height)
+                        cars[i].height +=10;
+                else if((cars[i].height + 5) <= src_img.size().height)
+                        cars[i].height += 5;
+
 
 			Point pt1(cars[i].x + cars[i].width, cars[i].y+cars[i].height);
 			Point pt2(cars[i].x, cars[i].y);
@@ -107,36 +141,41 @@ cout<<"height "<< src_img.size().height << endl;
 //				faces[i].height+=20;
 //
 //				//Grab faces in sepaate matrix
-//				Mat crop_img = src_img(faces[i]);
+//				Mat crop_img = src_img(cars[i]);
 //
-//				imshow("Crop", crop_img);
+//				imshow("Crop", src_img(cars[i]));
 //				//Write in jpg file grabbed face
-//				faceCount +=1;
-//				sprintf(imageToSave,"%s//face%d.jpg",folderName.c_str(),faceCount);
-//				imwrite(imageToSave, crop_img);
+
+if(cars[i].x <= (src_img.size().width - cars[i].width) && cars[i].y <= (src_img.size().height - cars[i].height))
+{
+				imshow("Crop", src_img(cars[i]));
+				CarCount +=1;
+				sprintf(imageToSave,"%s//car_plate_%d.jpg",folderName.c_str(),CarCount);
+				imwrite(imageToSave, src_img(cars[i]));
+}
 //
 //
 //
 				//                if(!eyes.empty())i
 				//To show the rectangle on face
-				rectangle(resize_src_img, pt1, pt2, Scalar(0,255,0),2, 8, 0);
+				rectangle(src_img, pt1, pt2, Scalar(0,255,0),2, 8, 0);
 	//		}
 		}
 #endif
-//		if(!src_img.empty())
-			imshow("Result", resize_src_img);
+		if(!src_img.empty())
+			imshow("Result", src_img);
 
-		waitKey(30);
+		waitKey(1);
 		//	int c = waitKey(30);
 		//	if((char)c == 37)
 		//	break;
 	}						
 
 	//destroy GUI windows
-//	destroyAllWindows();
+	destroyAllWindows();
 
 	//delete capture object
-//	capture.release();
-                waitKey(0);
+	capture.release();
+//                waitKey(0);
 	return 0;	
 	}
